8ca211489d7bb6c0c60d4c9b5485b9989f533da4
Radek Włodarski <wlodarski.radek@gmail.com>
2023-04-03 15:14:51 +0200

    added footer styling & mail icon functionality

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,18 +1,21 @@
 import "./style.css";
 import envelope from "../envelope.png";
 
-const Footer = () => (
-  <footer className="footer">
-    © 2023 - copyright Radosław Włodarski
-    <br />
+const Footer = () => {
+  return (
+    <footer className="footer">
+      © 2023 - copyright Radosław Włodarski
+      <br />
       <img
         className="footer__icon"
         width="40"
         height="24"
         src={envelope}
         alt="mail_me"
+        onClick={() => (window.location = "mailto:wlodarski.radek@gmail.com")}
       />
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
