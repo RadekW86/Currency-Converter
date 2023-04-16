bb2e454e076f510078cdb0b9168b1f9b4bf8a760
Radek Włodarski <wlodarski.radek@gmail.com>
2023-04-16 13:46:20 +0200

    simplified styling

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,9 +1,16 @@
-import {StyledSection} from "./styled.js";
+import styled from "styled-components";
 
-const Section = (props) => (
-    <StyledSection>
-        {props.children}
-    </StyledSection>
-);
+const Section = styled.section`
+  text-align: center;
+  background-color: ${({ theme }) => theme.backgroundColor};
+  margin: 10px;
+  padding: 20px;
+  border: 2px dotted #222;
+  border-radius: 30px;
+
+  @media (max-width: 767px) {
+    max-width: 350px;
+  }
+`;
 
 export default Section;
